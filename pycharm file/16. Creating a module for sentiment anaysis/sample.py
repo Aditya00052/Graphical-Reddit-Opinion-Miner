4fdd9392597deb63dbcf6f0f8a78bee29e901f44
Aditya00052 <111888942+Aditya00052@users.noreply.github.com>
2024-09-22 11:52:06 +0530

    Delete pycharm file directory

--- pycharm file/16. Creating a module for sentiment anaysis/sample.py
+++
@@ -1,6 +0,0 @@
-import sentiment_mod as s
-
-text = "It's clear that instead of fleshing out those scenes, the director Eli Roth, who also co-wrote the script with Joe Crombie, wanted to spend the film budget on the fight sequences. But he could have shaved those scenes down a little to make room. Scenes explaining the characters' backstories would have made a difference. In fact, just about anything would have made a difference in this long chain of shooting and blowing things up. As it stands, though, the movie feels like a string of sequences in a video game, but since you're not playing, it feels tedious instead of inspired, dull instead of gripping."
-
-print(s.sentiment(text))
-
